10caa5f9251ebeb576e13d898018f3c30f83f7c7
samnamlb <108905548+samnamlb@users.noreply.github.com>
2022-11-14 23:31:38 -0500

    Added student.py file
    Done with add_course() function

---
+++ student.py
@@ -0,0 +1,65 @@
+# ----------------------------------------------------------------
+# Author: Aleem Azimov
+# Date: 11/12/2022
+#
+# This module supports changes in the registered courses
+# for students in the class registration system.  It allows
+# students to add courses, drop courses and list courses they are
+# registered for.
+# -----------------------------------------------------------------
+
+def list_courses(id, c_roster):
+    # ------------------------------------------------------------
+    # This function displays and counts courses a student has
+    # registered for.  It has two parameters: id is the ID of the
+    # student; c_roster is the list of class rosters. This function
+    # has no return value.
+    # -------------------------------------------------------------
+    pass  # temporarily avoid empty function definition
+
+
+def add_course(id, c_roster, c_max_size):
+    # ------------------------------------------------------------
+    # This function adds a student to a course.  It has three
+    # parameters: id is the ID of the student to be added; c_roster is the
+    # list of class rosters; c_max_size is the list of maximum class sizes.
+    # This function asks user to enter the course he/she wants to add.
+    # If the course is not offered, display error message and stop.
+    # If student has already registered for this course, display
+    # error message and stop.
+    # If the course is full, display error message and stop.
+    # If everything is okay, add student ID to the course’s
+    # roster and display a message if there is no problem.  This
+    # function has no return value.
+    # -------------------------------------------------------------
+    while True:
+        course = input("Enter the course you would like: ")
+        if course not in c_roster:
+            print('Course not offered.')
+        elif id in c_roster[course]:
+            print('Student already registered for this course.')
+        elif len(c_roster[course]) >= c_max_size[course]:
+            print('Course is full.')
+        else:
+            print(c_roster[course])
+            print((c_max_size[course]))
+            break
+    course_list = c_roster[course]
+    course_list.append(id)
+    c_roster[course] = course_list
+
+    print(c_roster)
+
+
+def drop_course(id, c_roster):
+    # ------------------------------------------------------------
+    # This function drops a student from a course.  It has two
+    # parameters: id is the ID of the student to be dropped;
+    # c_roster is the list of class rosters. This function asks
+    # the user to enter the course he/she wants to drop.  If the course
+    # is not offered, display error message and stop.  If the student
+    # is not enrolled in that course, display error message and stop.
+    # Remove student ID from the course’s roster and display a message
+    # if there is no problem.  This function has no return value.
+    # -------------------------------------------------------------
+    pass  # temporarily avoid empty function definition
