85a448ca878a3d2005ed2a4fb19cbc96406d3888
samnamlb <108905548+samnamlb@users.noreply.github.com>
2022-11-15 16:19:15 -0500

    Removed while loops, readable code + comments

--- student.py
+++ student.py
@@ -13,7 +13,7 @@ def list_courses(id, c_roster):
     num_courses = 0
     # List of course code names
     course_names = []
-    # Add number of courses found where the student is registered in the roster
+    # Add number of courses
     for keys, value in c_roster.items():
         if id in value:
             course_names.append(keys)
@@ -27,6 +27,7 @@ def list_courses(id, c_roster):
     else:
         print(f'Registered to {num_courses} course')
     print('----------------------')
+
     # Display all registered courses
     for course in course_names:
         print(f'{course}')
@@ -34,39 +35,27 @@ def list_courses(id, c_roster):
 
 def add_course(id, c_roster, c_max_size):
     # Have user enter a course code
-    while True:
-        course = input("Enter the course you would like: ")
-        # When course is not found in the roster
-        if course not in c_roster:
-            print('Course not offered.')
-        # When the student is already found at the specified course
-        elif id in c_roster[course]:
-            print('Student already registered for this course.')
-        # When the course is full due to the max size from c_max_size dictionary
-        elif len(c_roster[course]) >= c_max_size[course]:
-            print('Course is full.')
-        else:
-            # Exit out of loop if no errors occurred
-            break
-    # Makes the value of the courses in the roster into an editable list
-    course_list = c_roster[course]
-    # Adds student to course
-    course_list.append(id)
-    # Puts the course list back as a value to the roster
-    c_roster[course] = course_list
+    course = input("Enter the course you would like: ")
+    if course not in c_roster:
+        print('Course not offered.')
+    elif id in c_roster[course]:
+        print('Student already registered for this course.')
+    elif len(c_roster[course]) >= c_max_size[course]:
+        print('Course is full.')
+    else:
+        # Add student to course
+        course_list = c_roster[course]
+        course_list.append(id)
+        c_roster[course] = course_list
 
 
 def drop_course(id, c_roster):
-    while True:
-        course = input("Enter the course you would like to drop: ")
-        # If course is not found in roster
-        if course not in c_roster:
-            print('Course not offered.')
-        # If the student based by id is not found in the roster
-        elif id not in c_roster[course]:
-            print('Student not in course.')
-        else:
-            # Exit out of loop if no errors occurred
-            break
-    # Remove student from the specified course
-    c_roster[course].remove(id)
+    # Drops courses
+    course = input("Enter the course you would like to drop: ")
+    if course not in c_roster:
+        print('Course not offered.')
+    elif id not in c_roster[course]:
+        print('Student not in course.')
+    else:
+        # Remove student from the specified course
+        c_roster[course].remove(id)
